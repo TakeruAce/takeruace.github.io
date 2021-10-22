691f66ac9d338101ce02ed89971c6f5d0c1deae6
Takeru Hashimoto <apple.takeru.0309@gmail.com>
2021-10-22 17:11:34 +0900

    update docs

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/var/folders/m6/m3jlq9ts25j_k4sly966_y940000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210425191248-0afcc81296de/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
